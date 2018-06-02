c8c8291ce3dcd2e7d630edff1f5ce48a15824ea0
MohdZin <MohdRashidi.MohdZin@gfk.com>
2018-06-03 06:02:08 +0800

    Initial commit
    Generated by Spring Initializr with :heart:

---
+++ src/main/java/rz/demo/boot/data/envers/DemoDataEnversApplication.java
@@ -0,0 +1,12 @@
+package rz.demo.boot.data.envers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDataEnversApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDataEnversApplication.class, args);
+    }
+}
